d2a6000969e4ae229917a3b49c13f81b42dc1cb8
daniel <danielrubensdaniel@gmail.com>
2023-06-12 10:25:24 -0300

    feat: child process with mongorestore

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const { exec } = require('child_process');
 const dotenv = require('dotenv')
+const fs = require('fs')
 dotenv.config()
 
+const path = './db';
 const { MONGO_URI } = process.env
 
-console.log(MONGO_URI)
+exec(`mongorestore --uri="${MONGO_URI}" --drop ${path}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
